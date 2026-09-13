4724ade9b90458706ba948fc7349743a6e581748
Hank0805 <0zf5g41738c559z@gmail.com>
2026-09-14 01:32:24 +0900

    Bump Mini OBS Studio Pro to v0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "jp.minobs.app"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.4.0"
     }
 
     compileOptions {
