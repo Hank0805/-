23a76feb86f72310aef1ef8df3780e55f288cfd0
Hank0805 <0zf5g41738c559z@gmail.com>
2026-09-14 02:20:40 +0900

    Set Mini OBS version 0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "jp.minobs.app"
         minSdk = 29
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.4.1"
+        versionCode = 6
+        versionName = "0.5.0"
     }
 
     compileOptions {
